f91737aa3584efe6ab926e48a1bab9a668e8b020
SportosVizilo <110971580+SportosVizilo@users.noreply.github.com>
2022-08-11 12:09:51 +0200

    Workshop site v1.0

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello Green Fox"); //string
+console.log(1700); //string
+console.log(65463 + 13267)
+console.log(65463 / 13267)
+console.log(65463 + 13267)
+console.log(65463 - 13267)
+console.log("This  number is: " + 13267)
+console.log(2 > 3);
+console.log(3 > 2);
+
+if (2 > 3) {
+  console.log("Two is indeed smaller then 3");
+}
+
+var isSmaller = 2 > 4; //true
+if(isSmaller) {
+  console.log("yep, it is");
+}else{
+  console.log("no, it is not");
+}
